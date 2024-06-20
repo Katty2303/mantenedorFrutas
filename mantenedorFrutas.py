f4bdb378a56b65f8e7c9fadab4de5d2229befec4
Katty2303 <ka.ramirezc@duocuc.cl>
2024-06-19 20:28:51 -0400

    Primera versión Mantenedor de Frutas
    Esta primera versión nos muestra un programa que permite ingresar frutas

---
+++ mantenedorFrutas.py
@@ -0,0 +1,33 @@
+import csv
+import os
+limpiarPantalla = "cls"
+minuscula = 0
+matrizFruta = []
+listaFruta = []
+opMenu = True
+
+while opMenu == True:
+    os.system(limpiarPantalla)
+    print("********** MENU MANTENEDOR DE FRUTA **********")
+    print("1. Ingresar fruta") # Creaciòn de un dato a la lista
+    print("2. Consulta") # Consulta que datos hay dentro de la matriz
+    print("3. Añadir fruta") # Agrega un nuevo dato a una matriz
+    print("4. Carga de datos") # (try) fruta.append = [nombre, unidad, cantidad, precio] / [pera, kilo, 10, 1500]
+    print("5. Guardado") # Guarda los datos a una matriz
+    print("6. Salida del sistema")
+    
+    try:
+        respuestaOp = int(input("\nIngrese su opciòn del menù: "))
+    except ValueError:
+        print("El valor ingresado no es correcto")
+    if opMenu == 1:
+        print("********** INGRESO DE FRUTAS **********")
+        ingresoFruta = str(input("Ingrese el nombre de la fruta que desea ingresar: "))
+        minuscula = ingresoFruta.lower()
+        listaFruta.append(minuscula)
+        opMenu = False
+        
+    elif opMenu == 2:
+        if listaFruta == " ":
+            print("no")
+        opMenu = False
